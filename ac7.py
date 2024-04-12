df4bc8049042d67dab0052b75d0818d896c448d6
Daniel Teixeira <127428783+DanielJT20@users.noreply.github.com>
2024-04-12 17:06:26 -0300

    ac7

---
+++ ac7.py
@@ -0,0 +1,42 @@
+
+"""
+Programação Estruturada
+2024.1
+12/04/2024
+
+Daniel de Jesus Teixeira
+Ciência de Dados e IA, turma: 8001
+
+Exercício 1 - AC7 
+"""
+
+# salario = float(input())
+# reajuste = 0
+
+# if(salario <= 400.00):
+#     reajuste = 15
+# elif(salario <= 800.00):
+#     reajuste = 12
+# elif(salario <= 1200.00):
+#     reajuste = 10
+# elif(salario <= 2000.00):
+#     reajuste = 7
+# else:
+#     reajuste = 4
+
+# print(f'Novo salario: {salario * (1 + reajuste/100):.2f}')
+# print(f'Reajuste ganho: {salario * reajuste/100:.2f}')
+# print(f'Em percentual: {reajuste} %')
+
+
+"""
+Programação Estruturada
+2024.1
+12/04/2024
+
+Daniel de Jesus Teixeira
+Ciência de Dados e IA, turma: 8001
+
+Exercício 2 - AC7 
+"""
+
